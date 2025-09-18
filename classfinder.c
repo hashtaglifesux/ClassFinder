b4b64676d04e845836e2982d01c1f51820a903cc
Katie <131010413+hashtaglifesux@users.noreply.github.com>
2025-09-18 13:43:34 -0400

    Add files via upload

---
+++ classfinder.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/stat.h>
+
+int isPast(char class_time[]) {
+  // current datetime as "weekday #, HH:MM"
+  time_t now;
+  struct tm ts;
+  time(&now);
+  ts = *localtime(&now);
+  char ftime[8];
+  strftime(ftime, 8, "%w %H:%M", &ts);
+  ftime[7] = 0;
+  
+  // read datetime from schedule in same format
+  char classtime_cpy[8];
+  memcpy(classtime_cpy, class_time, 7);
+  classtime_cpy[7] = 0;
+  
+  if (strcmp(class_time, ftime) > 0) {
+    return 0;
+  }
+  return 1;
+}
+
+
+int main() {
+  char fname[] = "/Users/adaaspirations/executables/classsheet.txt";
+  struct stat st;
+  stat(fname, &st);
+  int size = st.st_size;
+  
+  char* schedule = malloc(size);
+  FILE* fptr;
+  fptr = fopen(fname, "r");
+  fread(schedule, size, 1, fptr);
+  
+  uint64_t i = 0;
+  while (i < size-9) {
+    if (isPast(schedule+i) ) {
+      i = (uint64_t)(strstr(schedule+i, "\n\n") + 2 - schedule);
+    }
+    else {
+      i += 7;
+      uint64_t j = (uint64_t)(strstr(schedule+i, "\n\n") - schedule);
+      char* class_details = malloc(j-i);
+      memcpy(class_details, schedule+i, j-i);
+      printf("%s\n\n", class_details);
+      break;
+    }
+  }
+  
+  fclose(fptr);
+  return 0;
+}
